f70927db7b8cad849c423f9a7d51214001c1b9ed
Siege-Xcalibur <27.israel.olivera@ksd.org>
2025-11-03 13:14:17 -0800

    Enemy AI (I Guess)

--- 2D Team Project/Assets/Scenes/Scripts/Enemy AI.cs
+++ 2D Team Project/Assets/Scenes/Scripts/Enemy AI.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using UnityEditor.Timeline;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
